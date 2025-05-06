2348a0f431ec1ee0d9a1e8ad1ef6e8b3ae0f782c
Timeness <careless@flash.co>
2025-05-06 14:27:18 +0530

    done

--- app.py
+++ app.py
@@ -1,41 +1,18 @@
-from flask import Flask, request, render_template, jsonify, session
-import io, contextlib, traceback, logging
+from flask import Flask, request, render_template, jsonify
+import io, contextlib, traceback
 from flask_cors import CORS
-import requests
 import secrets
 
-myhexs = secrets.token_hex(32)
-
 app = Flask(__name__)
-app.secret_key = myhexs
+app.secret_key = secrets.token_hex(32)
 CORS(app)
-
-allowed_users = ["6505111743", "6517565595", "5896960462", "5220416927"]  # allowed Telegram user IDs
-
-# logging.basicConfig(filename='access_log.txt', level=logging.INFO)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/auth-telegram', methods=['POST'])
-def telegram_auth():
-    data = request.json
-    user = data.get('user')
-
-    ip = request.remote_addr
-    ua = request.headers.get('User-Agent')
-    # logging.info(f"[LOGIN] User: {user.get('id')} | IP: {ip} | UA: {ua}")
-
-    session['user_id'] = str(user.get('id'))
-    allowed = str(user.get('id')) in allowed_users
-    return jsonify({"allowed": allowed})
-
 @app.route('/eval', methods=['POST'])
 def eval_code():
-    #if 'user_id' not in session or session['user_id'] not in allowed_users:
-        #return jsonify({"result": "Unauthorized"})
-
     code = request.json.get('code')
     stdout = io.StringIO()
     try:
@@ -45,3 +22,6 @@ def eval_code():
     except Exception:
         result = traceback.format_exc()
     return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
